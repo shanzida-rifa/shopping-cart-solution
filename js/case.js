e50198894a0a1da855b73ae99fc811cbff7fba20
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-29 14:24:15 +0600

    added

--- js/case.js
+++ js/case.js
@@ -6,20 +6,39 @@
 5.set the value to the case number field
 internal
 */
-document.getElementById('btn-case-plus').addEventListener('click', function () {
+
+function updateCaseNumber(isIncrease) {
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
     console.log(previousCaseNumber);
 
-    const newCaseNumber = previousCaseNumber + 1;
+    let newCaseNumber;
+    if (isIncrease) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
     caseNumberField.value = newCaseNumber;
-});
-document.getElementById('btn-case-minus').addEventListener('click', function () {
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
+    return newCaseNumber;
+}
 
-    const newCaseNumber = previousCaseNumber - 1;
-    caseNumberField.value = newCaseNumber;
+function updateCaseTotalPrice(newCaseNumber) {
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(true);
+    updateCaseTotalPrice(newCaseNumber);
+
+});
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+
 })
---
+++ js/phone.js
@@ -0,0 +1,44 @@
+/*
+1.add event listener to the caseplus button
+2.get the value inside the case number field (input field)
+3. convert the number to an integer
+4.calculate the new case number
+5.set the value to the case number field
+internal
+*/
+
+function updatePhoneNumber(isIncrease) {
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+    console.log(previousPhoneNumber);
+
+    let newPhoneNumber;
+    if (isIncrease) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+    phoneNumberField.value = newPhoneNumber;
+    return newPhoneNumber;
+}
+
+function updatePhoneTotalPrice(newPhoneNumber) {
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+
+}
+
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+    const newPhoneNumber = updatePhoneNumber(true);
+    updatePhoneTotalPrice(newPhoneNumber);
+
+});
+
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
+    const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
+
+})
